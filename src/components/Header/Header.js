17fe20f67f6a73df17128d4d83009856deb50194
rezwan <rezwan6@gmail.com>
2021-11-30 00:38:25 +0600

    added links to icons. resume button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
                             <Nav.Link className="text-light" as={Link} to="/home">Home</Nav.Link>
                             <Nav.Link className="text-light" as={Link} to="/explore">Explore</Nav.Link>
                             <Nav.Link className="text-light" as={Link} to="/dashboard">Dashboard</Nav.Link>
+                            <a target="_blank" rel="noreferrer" className="btn btn-outline-info" href="https://drive.google.com/file/d/1Bj5lP8pf-SmNYj5Q_wxbeHvG91n7eHHE/view?usp=sharing">RESUME</a>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
