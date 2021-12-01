b88f8fb7d78834b8027bfee407107ea548f5b5f4
rezwan <rezwan6@gmail.com>
2021-12-01 19:37:21 +0600

    contact me page is created

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
+import './Contact.css';
 
 const Contact = () => {
     function sendEmail(e) {
@@ -15,31 +16,34 @@ const Contact = () => {
     }
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-md-6 mx-auto">
-                    <h2>Contact Form</h2>
-                    <form className="d-flex flex-column" onSubmit={sendEmail}>
-                        <label>Name</label>
-                        <input type="text" name="name" className="form-control" placeholder="Full Name" />
+        <div className="bg-dark text-light">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6 mx-auto form-container">
+                        <h3 className="text-warning">CONTACT ME</h3>
+                        <form className="d-flex flex-column" onSubmit={sendEmail}>
+                            <label className="mb-2 mt-3">Name</label>
+                            <input type="text" name="name" className="form-control" placeholder="Full Name" />
 
-                        <label>Email</label>
-                        <input type="email" name="user_email" className="form-control" placeholder="Email Address" />
+                            <label className="mb-2 mt-3">Email</label>
+                            <input type="email" name="user_email" className="form-control" placeholder="Email Address" />
 
-                        <label>Subject</label>
-                        <input type="text" name="subject" className="form-control" placeholder="Subject" />
+                            <label className="mb-2 mt-3">Subject</label>
+                            <input type="text" name="subject" className="form-control" placeholder="Subject" />
 
-                        <label>Message</label>
-                        <textarea name="message" rows="4" className="form-control" placeholder="Your Message" />
+                            <label className="mb-2 mt-3">Message</label>
+                            <textarea name="message" rows="4" className="form-control" placeholder="Your Message" />
 
-                        <input
-                            type="submit"
-                            value="Send"
-                            className="form-control btn btn-warning"
-                            style={{ marginTop: "30px" }}
-                        />
-                    </form>
-                    <p><em>sending to rezwan6@gmail.com</em></p>
+                            <input
+                                type="submit"
+                                value="Send"
+                                className="form-control btn btn-warning"
+                                style={{ marginTop: "30px" }}
+                            />
+                        </form>
+
+                        <p className="pt-3"><em>Sending to: <a style={{ color: 'turquoise' }} target="_blank" rel="noreferrer" href="mailto:rezwan6@gmail.com">rezwan6@gmail.com</a></em></p>
+                    </div>
                 </div>
             </div>
         </div>
