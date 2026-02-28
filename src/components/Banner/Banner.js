0e41867fac6dbefb582918f6978844e7a6b57dfa
Rezwanur Rakib Chy <rezwan6@gmail.com>
2026-02-28 19:30:39 +0600

    updated banner text

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -9,8 +9,8 @@ const Banner = () => {
                 <div className="col-12 col-md-6">
                     <h4>Hello, I am</h4>
                     <h1>Rezwanur Rakib Chy</h1>
-                    <h4>React Developer</h4>
-                    <p>I'm a self-motivated developer. I like to explore technologies and found web development as my passion. While in love with JavaScript, I try to learn and adapt to popular frameworks, libraries, and packages in order to keep pace with the changing web environment. My strength is that I like to take challenges and improve my skills continuously.</p>
+                    <h4>Backend-Focused Full-Stack Developer</h4>
+                    <p>Self-motivated developer passionate about building reliable and scalable web applications. Strong focus on backend systems with JavaScript and modern frameworks, continuously improving through real-world challenges and evolving technologies.</p>
                     <div style={{ marginTop: "40px", marginBottom: "40px" }}>
                         <a target="_blank" rel="noreferrer" href="https://github.com/rezbd"><i className="text-info fs-3 me-3 fab fa-github-square"></i></a>
                         <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/rezbd/"><i className="text-info fs-3 me-3 fab fa-linkedin"></i></a>
